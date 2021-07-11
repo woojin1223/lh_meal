d8472022fa1e2d88dc7f7589813a002f2157022b
WooJin1993 <ahnwj2004@gmail.com>
2021-07-11 19:08:04 +0900

    set variable names in uniform

--- make_features_wj.R
+++ make_features_wj.R
@@ -9,65 +9,65 @@ source("./functions.R")
 train <- read_csv("./data/train.csv")
 
 train %<>% rename(
-    date      = 일자,
-    dow       = 요일,
-    total     = 본사정원수,
-    vacation  = 본사휴가자수,
-    business  = 본사출장자수,
-    overtime  = 본사시간외근무명령서승인건수,
-    home      = 현본사소속재택근무자수,
-    breakfast = 조식메뉴,
-    lunch     = 중식메뉴,
-    dinner    = 석식메뉴,
-    lunch_n   = 중식계,
-    dinner_n  = 석식계
+    Date      = 일자,
+    Dayofweek = 요일,
+    Total     = 본사정원수,
+    Vacation  = 본사휴가자수,
+    Business  = 본사출장자수,
+    Overtime  = 본사시간외근무명령서승인건수,
+    Home      = 현본사소속재택근무자수,
+    Breakfast = 조식메뉴,
+    Lunch     = 중식메뉴,
+    Dinner    = 석식메뉴,
+    N_lunch   = 중식계,
+    N_dinner  = 석식계
 )
 
-train %<>% replace_na(list(dinner = ""))
+train %<>% replace_na(list(Dinner = ""))
 
 train %<>% mutate(
-    dow             = factor(dow, levels = c("월", "화", "수", "목", "금"), labels = c(1, 2, 3, 4, 5)),
-    net_total       = total - (vacation+business+home),
-    lunch_rate      = lunch_n / net_total,
-    dinner_rate     = dinner_n / net_total,
-    new_lunch_menu  = get_new_menu(lunch),
-    new_dinner_menu = get_new_menu(dinner)
+    Dayofweek   = factor(Dayofweek, levels = c("월", "화", "수", "목", "금"), labels = c(1, 2, 3, 4, 5)),
+    Net_total   = Total - (Vacation+Business+Home),
+    Rate_lunch  = N_lunch / Net_total,
+    Rate_dinner = N_dinner / Net_total,
+    New_lunch   = get_new_menu(Lunch),
+    New_dinner  = get_new_menu(Dinner)
 )
 
-lunch_score_dict <- get_score_dict(train$lunch, train$lunch_rate)
-dinner_score_dict <- get_score_dict(train$dinner, train$dinner_rate)
+lunch_score_dict <- get_score_dict(train$Lunch, train$Rate_lunch)
+dinner_score_dict <- get_score_dict(train$Dinner, train$Rate_dinner)
 
 train %<>% mutate(
-    lunch_score  = get_score(lunch, lunch_score_dict),
-    dinner_score = get_score(dinner, dinner_score_dict)
+    Lunch_score  = get_score(Lunch, lunch_score_dict),
+    Dinner_score = get_score(Dinner, dinner_score_dict)
 )
 
 write_csv(train, path = "./data/train_wj.csv")
 
 test <- read_csv("./data/test.csv")
 test %<>% rename(
-    date      = 일자,
-    dow       = 요일,
-    total     = 본사정원수,
-    vacation  = 본사휴가자수,
-    business  = 본사출장자수,
-    overtime  = 본사시간외근무명령서승인건수,
-    home      = 현본사소속재택근무자수,
-    breakfast = 조식메뉴,
-    lunch     = 중식메뉴,
-    dinner    = 석식메뉴
+    Date      = 일자,
+    Dayofweek = 요일,
+    Total     = 본사정원수,
+    Vacation  = 본사휴가자수,
+    Business  = 본사출장자수,
+    Overtime  = 본사시간외근무명령서승인건수,
+    Home      = 현본사소속재택근무자수,
+    Breakfast = 조식메뉴,
+    Lunch     = 중식메뉴,
+    Dinner    = 석식메뉴
 )
 
 test %<>% mutate(
-    dow             = factor(dow, levels = c("월", "화", "수", "목", "금"), labels = c(1, 2, 3, 4, 5)),
-    net_total       = total - (vacation+business+home),
-    new_lunch_menu  = get_new_menu(lunch),
-    new_dinner_menu = get_new_menu(dinner)
+    Dayofweek  = factor(Dayofweek, levels = c("월", "화", "수", "목", "금"), labels = c(1, 2, 3, 4, 5)),
+    Net_total  = Total - (Vacation+Business+Home),
+    New_lunch  = get_new_menu(Lunch),
+    New_dinner = get_new_menu(Dinner)
 )
 
 test %<>% mutate(
-    lunch_score  = get_score(lunch, lunch_score_dict),
-    dinner_score = get_score(dinner, dinner_score_dict)
+    Lunch_score  = get_score(Lunch, lunch_score_dict),
+    dinner_score = get_score(Dinner, dinner_score_dict)
 )
 
 write_csv(test, path = "./data/test_wj.csv")
