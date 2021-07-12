3d2ad04116543037932b06822fd6b20df768ae6d
WooJin1993 <ahnwj2004@gmail.com>
2021-07-12 13:07:23 +0900

    modify merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -1,9 +1,14 @@
+library(readr)
+library(dplyr)
+
+train_wj <- read_csv("./data/train_wj.csv")
+test_wj <- read_csv("./data/test_wj.csv")
+
 train_jh <- read_csv("./data/train_jh.csv")
 test_jh <- read_csv("./data/test_jh.csv")
 
-train_wj <- bind_cols(train, train_jh[c("Before_holiday", "After_holiday", "Sandwich_day")])
-test_wj <- bind_cols(test, test_jh[c("Before_holiday", "After_holiday", "Sandwich_day")])
-
+train_wj <- bind_cols(train_wj, train_jh[c("Before_holiday", "After_holiday", "Sandwich_day")])
+test_wj <- bind_cols(test_wj, test_jh[c("Before_holiday", "After_holiday", "Sandwich_day")])
 
 write_csv(train_wj, path = "./data/train_wj.csv")
 write_csv(test_wj, path = "./data/test_wj.csv")
